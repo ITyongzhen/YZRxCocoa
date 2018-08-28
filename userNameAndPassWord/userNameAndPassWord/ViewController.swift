e341d13db67f79e652481560fd7bd78ec5d802d9
yongzhen <eagle_01yz@163.com>
2018-08-28 19:23:54 +0800

    userNamePassWord

---
+++ userNameAndPassWord/userNameAndPassWord/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  userNameAndPassWord
+//
+//  Created by yongzhen on 2018/8/28.
+//  Copyright © 2018年 yongzhen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var passWordTextField: UITextField!
+    
+    @IBOutlet weak var passWordLabel: UILabel!
+    
+    
+    @IBOutlet weak var button: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
