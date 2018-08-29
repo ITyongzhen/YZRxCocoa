33740c5102bbb05c558207486544e36e96f04b67
yongzhen <eagle_01yz@163.com>
2018-08-29 11:50:15 +0800

    这是一个模拟用户登录的程序。
    当用户输入用户名时，如果用户名不足 5 个字就给出红色提示语，并且无法输入密码，当用户名符合要求时才可以输入密码。
    同样的当用户输入的密码不到 5 个字时也给出红色提示语。
    当用户名和密码有一个不符合要求时底部的按钮不可点击，只有当用户名和密码同时有效时按钮才可点击。
    当点击按钮后弹出一个提示框，这个提示框只是用来做演示而已。

--- userNameAndPassWord/userNameAndPassWord/ViewController.swift
+++ userNameAndPassWord/userNameAndPassWord/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
     @IBOutlet weak var userNameTextField: UITextField!
@@ -20,11 +22,86 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var button: UIButton!
     
-    
+      let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let minUserNameLength = 5
+        let minPassWordLength = 6
+        
+        userNameLabel.text = "userName has to be at least \(minUserNameLength) characters"
+        passWordLabel.text = "passWord has to be at least \(minPassWordLength) characters"
+        // 用户名是否有效
+        let usernameValid = userNameTextField.rx.text.orEmpty.map{ $0.count >= minUserNameLength}.share(replay: 5)
+        // 密码是否有效
+        let passWordValid = passWordTextField.rx.text.orEmpty.map{ $0.count >= minPassWordLength}.share(replay: 1)
+        
+         // 所有输入是否有效
+        let everythingValid = Observable.combineLatest(usernameValid,passWordValid){ $0 && $1 }.share(replay: 1)
+        
+        // 用户名是否有效 -> 密码输入框是否可用
+        usernameValid.bind(to: passWordTextField.rx.isEnabled).disposed(by: disposeBag)
+        // 用户名是否有效 -> 用户名提示语是否隐藏
+        usernameValid.bind(to: userNameLabel.rx.isHidden).disposed(by: disposeBag)
+        // 密码是否有效 -> 密码提示语是否隐藏
+        passWordValid.bind(to: passWordLabel.rx.isHidden).disposed(by: disposeBag)
+        
+         // 所有输入是否有效 -> 按钮是否可点击
+        everythingValid.bind(to: button.rx.isEnabled).disposed(by: disposeBag)
+        //点击按钮 -> 弹出提示框
+        button.rx.tap.subscribe(onNext: {[weak self] in self?.showAlert()}).disposed(by: disposeBag)
+    }
+    func putIntUserNameAndPassWord() {
+        let minimalUsernameLength = 5
+        let minimalPasswordLength = 6
         // Do any additional setup after loading the view, typically from a nib.
+        userNameLabel.text = "Username has to be at least \(minimalUsernameLength) characters"
+        passWordLabel.text = "Password has to be at least \(minimalPasswordLength) characters"
+        
+        let usernameValid = userNameTextField.rx.text.orEmpty
+            .map { $0.count >= minimalUsernameLength }
+            .share(replay: 1)
+        
+        let passwordValid = passWordTextField.rx.text.orEmpty
+            .map { $0.count >= minimalPasswordLength }
+            .share(replay: 1)
+        
+        let everythingValid = Observable.combineLatest(
+            usernameValid,
+            passwordValid
+        ) { $0 && $1 }
+            .share(replay: 1)
+        
+        usernameValid
+            .bind(to: passWordTextField.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        usernameValid
+            .bind(to: userNameLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        passwordValid
+            .bind(to: passWordLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        everythingValid
+            .bind(to: button.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        button.rx.tap
+            .subscribe(onNext: { [weak self] in self?.showAlert() })
+            .disposed(by: disposeBag)
+    }
+    
+    func showAlert() {
+        let alertView = UIAlertView(
+            title: "RxExample",
+            message: "This is wonderful",
+            delegate: nil,
+            cancelButtonTitle: "OK"
+        )
+        
+        alertView.show()
     }
 
     override func didReceiveMemoryWarning() {
